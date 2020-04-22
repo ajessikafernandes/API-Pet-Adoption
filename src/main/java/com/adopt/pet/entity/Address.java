88f4d641295cbed0aab592d377aa4f169765ca04
ajessikafernandes <jessika.alves.fernandes@outlook.com>
2020-04-21 21:44:59 -0300

    create entity Address

---
+++ src/main/java/com/adopt/pet/entity/Address.java
@@ -0,0 +1,36 @@
+package com.adopt.pet.entity;
+
+import com.adopt.pet.enums.State;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 9, nullable = false)
+    private String zip;
+    @Column(length = 40, nullable = false)
+    private String city;
+    @Column(length = 2, nullable = false)
+    private State uf;
+
+    @JsonIgnore
+    @OneToOne
+    @PrimaryKeyJoinColumn
+    private Pet pet;
+
+    @JsonIgnore
+    @OneToOne
+    @PrimaryKeyJoinColumn
+    private User user;
+}
