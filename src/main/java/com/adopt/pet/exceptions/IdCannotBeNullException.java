9c66ae0381d1ea2529d3485484ed727785e6ef78
ajessikafernandes <jessika.alves.fernandes@outlook.com>
2020-04-29 22:30:19 -0300

    new exceptions for id null and pet null

---
+++ src/main/java/com/adopt/pet/exceptions/IdCannotBeNullException.java
@@ -0,0 +1,9 @@
+package com.adopt.pet.exceptions;
+
+public class IdCannotBeNullException extends RuntimeException {
+
+    public IdCannotBeNullException(String message) {
+        super(message);
+    }
+
+}
---
+++ src/main/java/com/adopt/pet/exceptions/PetCannotBeNullException.java
@@ -0,0 +1,9 @@
+package com.adopt.pet.exceptions;
+
+public class PetCannotBeNullException extends RuntimeException {
+
+    public PetCannotBeNullException(String message) {
+        super(message);
+    }
+
+}
