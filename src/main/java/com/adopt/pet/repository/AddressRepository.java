8b2ab604f1f3826ce60d112abbfab699d3d78cfa
ajessikafernandes <jessika.alves.fernandes@outlook.com>
2020-05-10 13:23:30 -0300

    adjust repository address

--- src/main/java/com/adopt/pet/repository/AddressRepository.java
+++ src/main/java/com/adopt/pet/repository/AddressRepository.java
@@ -4,8 +4,6 @@ import com.adopt.pet.entity.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
 
