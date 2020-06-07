6a0473ed161f403cc75051162a82f1936e7c404d
ajessikafernandes <jessika.alves.fernandes@outlook.com>
2020-06-07 19:49:32 -0300

    add test Select Interview Enum

---
+++ src/test/java/com/adopt/pet/enums/SelectInterviewEnumTest.java
@@ -0,0 +1,38 @@
+package com.adopt.pet.enums;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+public class SelectInterviewEnumTest {
+
+    @Test
+    public void testWithValueInvalid() {
+        assertNull(SelectInterviewEnum.byValue("INVALID"));
+    }
+
+    @Test
+    public void testWithValueValid() {
+        assertEquals(SelectInterviewEnum.valueOf("YES"), SelectInterviewEnum.YES);
+    }
+
+    @Test
+    public void testWithValueNull() {
+        assertTrue(SelectInterviewEnum.optionIsNull(null));
+    }
+
+    @Test
+    public void testIgnoreCase(){
+        assertEquals(SelectInterviewEnum.byValue("yes"), SelectOptionEnum.YES);
+    }
+
+    @Test
+    public void testIgnoreDifferentCase(){
+        assertEquals(SelectInterviewEnum.byValue("Yes"), SelectOptionEnum.YES);
+    }
+
+}
