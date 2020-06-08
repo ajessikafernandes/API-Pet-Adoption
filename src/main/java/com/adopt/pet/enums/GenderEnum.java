15974c2ffb1d4ece004fc476dc8ffb0a7731f29e
ajessikafernandes <jessika.alves.fernandes@outlook.com>
2020-06-08 00:07:47 -0300

    refactor code in GenderEnum

--- src/main/java/com/adopt/pet/enums/GenderEnum.java
+++ src/main/java/com/adopt/pet/enums/GenderEnum.java
@@ -1,23 +1,23 @@
 package com.adopt.pet.enums;
 
+import java.util.Objects;
+
 public enum GenderEnum {
 
-    FEMALE("Female"),
-    MALE("Male");
+    FEMALE,
+    MALE;
 
-    private String value;
-
-    GenderEnum(String value) {
-        this.value = value;
+    public static GenderEnum byValue( String status ) {
+        for (GenderEnum type : GenderEnum.values()) {
+            if (type.name().equalsIgnoreCase(status)) {
+                return type;
+            }
+        }
+        return null;
     }
 
-    public String getValue() {
-        return value;
-    }
-
-    @Override
-    public String toString() {
-        return this.getValue();
+    public static boolean optionIsNull(GenderEnum genderEnum){
+        return Objects.isNull(genderEnum);
     }
 
 }
