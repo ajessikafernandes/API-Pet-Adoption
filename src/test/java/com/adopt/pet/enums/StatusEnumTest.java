c270833423b75418d9d22a6ac9afa0fcd2f186ef
ajessikafernandes <jessika.alves.fernandes@outlook.com>
2020-06-08 00:21:52 -0300

    created class test StatusEnumTest

--- src/test/java/com/adopt/pet/enums/StatusEnumTest.java
+++ src/test/java/com/adopt/pet/enums/StatusEnumTest.java
@@ -1,9 +1,39 @@
 package com.adopt.pet.enums;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
 
-class StatusEnumTest {
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+@RunWith(SpringRunner.class)
+public class StatusEnumTest {
+
+    @Test
+    public void testWithValueInvalid() {
+        assertNull(StatusEnum.byValue("INVALID"));
+    }
+
+    @Test
+    public void testWithValueValid() {
+        assertEquals(StatusEnum.valueOf("AVAILABLE"), StatusEnum.AVAILABLE);
+    }
+
+    @Test
+    public void testWithValueNull() {
+        assertTrue(StatusEnum.optionIsNull(null));
+    }
+
+    @Test
+    public void testIgnoreCase(){
+        assertEquals(StatusEnum.byValue("available"), StatusEnum.AVAILABLE);
+    }
+
+    @Test
+    public void testIgnoreDifferentCase(){
+        assertEquals(StatusEnum.byValue("Available"), StatusEnum.AVAILABLE);
+    }
 
 
 }
